c7f1c930f352a4308e6a19f90199b4ce4c9f3b5b
ads-andredesantana <ads-andredesantana@gmail.com>
2021-01-07 15:50:39 -0500

    Creating a Context to be used with the Rooms

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react'
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa'
 import Title from './Title'
 
 
 export default class Services extends Component {
-    render() {
-        return (
-            <div>
-                hello services
-            </div>
-        )
-    }
+	state = {
+		services: [
+			{
+				icon: <FaCocktail />,
+				title: "Free cocktails",
+				info: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, fugit.'
+			},
+			{
+				icon: <FaHiking />,
+				title: "Endless Hiking",
+				info: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, fugit.'
+			},
+			{
+				icon: <FaShuttleVan />,
+				title: "Free Shuttle",
+				info: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, fugit.'
+			},
+			{
+				icon: <FaBeer />,
+				title: "Strongest Beer",
+				info: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, fugit.'
+			}
+		]
+	}
+	render() {
+		return (
+			<section className="services">
+				<Title title="services" />
+				<div className="services-center">
+					{this.state.services.map((item, index) => {
+						return <article key={index} className="service">
+							<span>{item.icon}</span>
+							<h6>{item.title}</h6>
+							<p>{item.info}</p>
+						</article>
+					})}
+				</div>
+			</section>
+		)
+	}
 }
