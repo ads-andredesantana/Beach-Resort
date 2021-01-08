5709a3da290725e808bf0e1f430e696d6347b649
ads-andredesantana <ads-andredesantana@gmail.com>
2021-01-08 12:35:03 -0500

    Creating the function getUnique to get unique values, store in a variable and use the values

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,9 +1,64 @@
 import React from 'react'
+import { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "../components/Title";
 
-export default function RoomFilter() {
+// Creating a Function to get all unique values
+const getUnique = (items, value) => {
+  // Using Ste because It only accepts unique values
+  return [...new Set(items.map(item => item[value]))];
+};
+
+export default function RoomFilter({ rooms }) {
+  const context = useContext(RoomContext);
+  // Using React Hooks to acces the object values
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
+
+  // get unique types
+  let types = getUnique(rooms, "type");
+
+  // add all
+  types = ["all", ...types];
+
+  // map to jsx
+  types = types.map((item, index) => {
+    return <option value={item} key={index}>{item}</option>
+  })
+
+
+
+
   return (
-    <div>
-      Hello From Room Filter
-    </div>
+    <section className="filter-container">
+      <Title title="search rooms" />
+
+      <form className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+        {/* end of select type */}
+      </form>
+    </section>
   )
 }
