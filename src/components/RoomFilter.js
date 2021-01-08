86583826f78fff28cabf0869cd581cf165d7c295
ads-andredesantana <ads-andredesantana@gmail.com>
2021-01-08 14:21:21 -0500

    Finishing the Extras Filter

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -122,6 +122,34 @@ export default function RoomFilter({ rooms }) {
         </div>
         {/* end of sizes Min & Max */}
 
+        {/* extras */}
+        <div className="form-group">
+
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="breakfast"
+              id="breakfast"
+              checked={breakfast}
+              onChange={handleChange}
+            />
+            <label htmlFor="breakfast">breakfast</label>
+          </div>
+
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="pets"
+              checked={pets}
+              onChange={handleChange}
+            />
+            <label htmlFor="pets">pets</label>
+          </div>
+
+        </div>
+        {/* end of extras type */}
+
+
       </form>
     </section>
   )
